a7f1098b5dfc2bea09a0824b1793b74b6a908300
tresvezesheber <hebireaper@gmail.com>
2024-03-01 09:50:29 -0300

    its alive

---
+++ src/main/java/br/dev/hebio/careerintershipcenterapi/CareerIntershipCenterApiApplication.java
@@ -0,0 +1,13 @@
+package br.dev.hebio.careerintershipcenterapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CareerIntershipCenterApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CareerIntershipCenterApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/dev/hebio/careerintershipcenterapi/CareerIntershipCenterApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.dev.hebio.careerintershipcenterapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CareerIntershipCenterApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
